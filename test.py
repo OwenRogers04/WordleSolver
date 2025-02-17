ab6beff29d9d7227b096bba3fa005ace9b372eb9
OwenRogers04 <owenrogers0819@gmail.com>
2025-02-17 14:50:59 -0500

    working intro and used.txt added

--- test.py
+++ test.py
@@ -4,10 +4,26 @@ import time
 
 webbrowser.open('https://www.nytimes.com/games/wordle/index.html')
 
-print(pyautogui.__version__)
+time.sleep(2)
 
-time.sleep(5)
-
-location = pyautogui.locateOnScreen('play.png')
-if location:
+location = pyautogui.locateOnScreen('images/play.png')
+if location is None:
+    print("Image not found on screen.")
+else:
     pyautogui.click(location)
+
+time.sleep(.5)
+
+location = pyautogui.locateOnScreen('images/X.png')
+if location is None:
+    print("Image not found on screen.")
+else:
+    pyautogui.click(location)
+
+time.sleep(.5)
+
+pyautogui.scroll(-500)
+
+time.sleep(.5)
+
+pyautogui.typewrite('arose')
